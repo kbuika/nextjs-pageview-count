36070ffc95576f98f5105c39b48f3ea4b960bbca
kibuikaCodes <kibuikasteve@gmail.com>
2023-12-09 23:28:21 +0300

    complete build

--- src/app/page3/page.tsx
+++ src/app/page3/page.tsx
@@ -1,16 +1,15 @@
-import { GetServerSideProps, NextPage } from "next";
 import { ReportView } from "@/components/report-view";
 import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
 
-export default async function Home() {
+export default async function Page() {
   const views =
-    (await redis.get<any>(["pageviews", "page", "page-1"].join(":"))) ?? 0;
+    (await redis.get<any>(["pageviews", "page", "page-3"].join(":"))) ?? 0;
 
-    return (
+  return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ReportView slug={"page-1"} />
+      <ReportView slug={"page-3"} />
 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -26,10 +25,10 @@ export default async function Home() {
       </div>
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="text-4xl">Page 1</h1>
+        <h1 className="text-4xl">Page 3</h1>
         <p className="mt-4 text-2xl">
-          &quot;When I don&apos;t like something about the world, I change
-          it&quot; ~ Rick
+          &quot;Nobody belongs anywhere, Everybody&apos;s gonna die. Come watch
+          TV&quot; ~ Rick
         </p>
         <p className="mt-4 text-xl">{views} views</p>
       </div>
